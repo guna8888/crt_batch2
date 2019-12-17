3de91022b07596b62eadb8088e588ce412a0729d
guna8888 <58871093+guna8888@users.noreply.github.com>
2019-12-17 12:18:49 +0530

    Add files via upload

---
+++ Day4 (1).py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # tuples: 
+
+# ## tuples are enclosed in parathesis()
+# ##
+# 
+
+# # diff between lists and
+
+# In[1]:
+
+
+tuple1 = (1,3,4,12.4,"python")
+print(tuple1)
+type(tuple1)
+
+
+# In[7]:
+
+
+tuple1[0:3]
+
+
+# # dictionaries 
+
+# ### all the data will be enclosed in the {}
+# ### the data will be in the form of key values pairs
+# ### each key is seperated with its value by a colon(:)
+# ### each key is value pair is seperated by a comma(,)
+# ### keys are unique 
+
+# In[11]:
+
+
+di = {1: 'gitam',2: 'hyd'}
+print(di)
+type(di)
+
+
+# In[27]:
+
+
+dict1={'name':'gitam ','email':'gitam@123.com','adddress':'hyderbad',}
+
+print(dict1)
+
+
+# In[25]:
+
+
+dict1.keys()
+
+
+# In[26]:
+
+
+dict1
+
+
+# In[28]:
+
+
+dict1.values()
+
+
+# In[29]:
+
+
+dict1.items()
+
+
+# In[ ]:
+
+
+
+
